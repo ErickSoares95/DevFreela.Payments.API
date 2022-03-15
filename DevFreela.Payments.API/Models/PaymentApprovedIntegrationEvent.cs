d73b73f5703007da43a5e9ea83a344f13838a435
ErickSoares95 <ericklira99@gmail.com>
2022-03-15 18:57:08 -0300

    payment microservice for DevFreela.API

---
+++ DevFreela.Payments.API/Models/PaymentApprovedIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Models
+{
+    public class PaymentApprovedIntegrationEvent
+    {
+        public PaymentApprovedIntegrationEvent(int IdProject)
+        {
+            IdProject = IdProject;
+        }
+
+        public int IdProject { get; set; }
+    }
+}
